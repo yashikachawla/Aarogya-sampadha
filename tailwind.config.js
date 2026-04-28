7579444fec637bc190d169f0d314561050c1439d
yashikachawla <126230119+yashikachawla@users.noreply.github.com>
2026-04-28 14:22:17 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#d91b5c",
+        bgLight: "#f6f7fb",
+        textDark: "#1f2937",
+        textLight: "#6b7280",
+      },
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
